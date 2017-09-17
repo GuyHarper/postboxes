b6b0d8a2ba3ca0f0a26eaeaf44ac056f379284f3
= <guy.harper@gmail.com>
2017-09-17 14:35:39 +0100

    Hooking up mongoose

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
 const router = require('./config/router');
+
+const { dbURI, port } = require('./config/environment');
+
+mongoose.connect(dbURI, { useMongoClient: true });
 
 app.use(router);
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(port, () => console.log(`Express is up and running on port ${port}`));
