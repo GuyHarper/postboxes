b30436bd5c33850267fde7d76ca29c2fcc231df7
= <guy.harper@gmail.com>
2017-09-17 14:38:38 +0100

    Setting up globalToJSON mongoose plugin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+mongoose.plugin(require('./lib/globalToJSON'));
 
 const router = require('./config/router');
 
