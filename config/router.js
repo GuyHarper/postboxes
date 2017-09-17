3f34f007f25b7054f3c7622b45eb8d1a685b0f46
= <guy.harper@gmail.com>
2017-09-17 15:55:15 +0100

    Building out routes

--- config/router.js
+++ config/router.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const postboxes = require('../controllers/postboxes');
 
 router.get('/', (req, res) => res.send('HOME'));
+
+router.route('/postboxes')
+  .get(postboxes.index)
+  .post(postboxes.create);
+
+router.route('/postboxes/:id')
+  .get(postboxes.show)
+  .put(postboxes.update)
+  .delete(postboxes.delete);
 
 module.exports = router;
--- controllers/postboxes.js
+++ controllers/postboxes.js
@@ -1,1 +1,55 @@
 const Postbox = require('../models/postbox');
+
+function postboxesIndex(req, res) {
+  Postbox
+    .find().limit(5)
+    .exec()
+    .then(postboxes => res.json(postboxes))
+    .catch(err => res.status(500).json(err));
+}
+
+function postboxesShow(req, res) {
+  Postbox
+    .findById(req.params.id)
+    .exec()
+    .then(postbox => res.json(postbox))
+    .catch(err => res.status(500).json(err));
+}
+
+function postboxesCreate(req, res) {
+  Postbox
+    .create(req.body)
+    .then(postbox => res.status(201).json(postbox))
+    .catch(err => res.status(500).json(err));
+}
+
+function postboxesUpdate(req, res) {
+  Postbox
+    .findById(req.params.id)
+    .exec()
+    .then(postbox => {
+      postbox = Object.assign(postbox, req.body);
+      return postbox.save();
+    })
+    .then(postbox => res.json(postbox))
+    .catch(err => res.status(500).json(err));
+}
+
+function postboxesDelete(req, res) {
+  Postbox
+    .findById(req.params.id)
+    .exec()
+    .then(postbox => {
+      postbox.remove();
+    })
+    .then(() => res.status(204).json())
+    .catch(err => res.status(500).json(err));
+}
+
+module.exports = {
+  index: postboxesIndex,
+  show: postboxesShow,
+  create: postboxesCreate,
+  update: postboxesUpdate,
+  delete: postboxesDelete
+};
