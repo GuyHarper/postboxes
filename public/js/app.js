9dc60d029cf0d6d254523a2f4e7ca25bed62cd0d
= <guy.harper@gmail.com>
2017-09-17 19:38:41 +0100

    Tidying up data

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ $(() => {
 
   const $map = $('.map');
   let map = null;
+  let locations = null;
 
   function initializeMap() {
     const latLng = { lat: 51.515213, lng: -0.072331 };
@@ -13,5 +14,24 @@ $(() => {
     });
   }
 
+  function getPostboxes() {
+  $.get('http://localhost:3000/postboxes')
+    .done((response) => {
+      locations = response;
+      locations.forEach((location) => {
+        showMarker(location);
+      });
+    });
+  }
+
+  function showMarker(location) {
+    const latLng = { lat: location.lat, lng: location.lng };
+    const marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+
   initializeMap();
+  getPostboxes();
 });
