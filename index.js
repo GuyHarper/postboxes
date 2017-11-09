1fee51a5412293b80e8dfae51534de6a01426955
= <guy.harper@gmail.com>
2017-11-09 17:20:00 +0000

    Setting up for React port

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.plugin(require('./lib/globalToJSON'));
 mongoose.Promise = require('bluebird');
+mongoose.plugin(require('./lib/globalToJSON'));
+mongoose.plugin(require('mongoose-unique-validator'));
+
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
-const router = require('./config/router');
-
-const { dbURI, port } = require('./config/environment');
+const { port, dbURI, env } = require('./config/environment');
+const customResponses = require('./lib/customResponses');
+const errorHandler = require('./lib/errorHandler');
 
 mongoose.connect(dbURI, { useMongoClient: true });
 
-app.use(morgan('dev'));
+if('test' !== env) app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 
-app.use(router);
+app.use(customResponses);
 
-app.listen(port, () => console.log(`Express is up and running on port ${port}`));
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
+
+module.exports = app;
