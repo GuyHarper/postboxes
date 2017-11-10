a1e9af0f0039db6a88ef72422e82e87b20c77600
= <guy.harper@gmail.com>
2017-11-10 13:38:20 +0000

    Setting up infowindow functionality

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,8 @@ import Axios from 'axios';
 class Map extends React.Component {
   state = {
     postboxes: [],
-    locality: { lat: 51.515213, lng: -0.072331 }
+    locality: { lat: 51.515213, lng: -0.072331 },
+    infowindows: []
   }
 
   componentDidMount() {
@@ -24,21 +25,45 @@ class Map extends React.Component {
     });
   }
 
-  showMarker = (location) => {
-    const latLng = { lat: location.lat, lng: location.lng };
-    this.marker = new google.maps.Marker({
+  showMarker = (postbox) => {
+    console.log(postbox);
+    const latLng = { lat: postbox.lat, lng: postbox.lng };
+    const marker = new google.maps.Marker({
       position: latLng,
       map: this.map,
       icon: '../assets/images/red-dot.svg'
     });
+    let content = null;
+    if(postbox.lastCollection && postbox.saturdayCollection) {
+      content = `<p><strong>Last collection:</strong> ${postbox.lastCollection}</p><p><strong>Saturday collection:</strong> ${postbox.saturdayCollection}</p>`;
+    } else {
+      content = `<p><strong>Last collection:</strong> ${postbox.lastCollection}</p>`;
+    }
+    const info = `<div id="content"><div id="bodyContent">${content}</div></div>`;
+    if(postbox.lastCollection) {
+      const infowindow = new google.maps.InfoWindow({
+        content: info
+      });
+      marker.addListener('click', () => {
+        this.state.infowindows.forEach((infowindow) => {
+          infowindow.close();
+        });
+        infowindow.open(this.map, marker);
+      });
+      this.setState(prevState => {
+        const infowindows = [...prevState.infowindows];
+        infowindows.push(infowindow);
+        return {infowindows: infowindows};
+      }, () => console.log(this.state));
+    }
   }
 
   findNearby = () => {
-    const nearbyPostboxLocations = this.state.postboxes.filter((postbox) => {
+    const nearbyPostboxes = this.state.postboxes.filter((postbox) => {
       return (Math.abs(postbox.lng - this.state.locality.lng) < 0.005) && (Math.abs(postbox.lat - this.state.locality.lat) < 0.005);
     });
-    nearbyPostboxLocations.forEach((location) => {
-      this.showMarker(location);
+    nearbyPostboxes.forEach((postbox) => {
+      this.showMarker(postbox);
     });
   }
 
