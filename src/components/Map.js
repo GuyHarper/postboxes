03e03165d39f9d10d7e6f89ade3879f7acea0dd3
= <guy.harper@gmail.com>
2017-11-10 12:50:43 +0000

    Getting nearby postboxes to display

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,6 +9,14 @@ class Map extends React.Component {
     locality: { lat: 51.515213, lng: -0.072331 }
   }
 
+  componentDidMount() {
+    this.initializeMap();
+    Axios
+      .get('/api/postboxes')
+      .then(res => this.setState({ postboxes: res.data }, () => this.findNearby()))
+      .catch(err => console.log(err));
+  }
+
   initializeMap = () => {
     this.map = new google.maps.Map(this.mapCanvas, {
       zoom: 14,
@@ -16,11 +24,21 @@ class Map extends React.Component {
     });
   }
 
-  componentWillMount() {
-    Axios
-      .get('/api/postboxes')
-      .then(res => this.setState({ postboxes: res.data }, () => this.initializeMap()))
-      .catch(err => console.log(err));
+  showMarker = (location) => {
+    const latLng = { lat: location.lat, lng: location.lng };
+    this.marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map
+    });
+  }
+
+  findNearby = () => {
+    const nearbyPostboxLocations = this.state.postboxes.filter((postbox) => {
+      return (Math.abs(postbox.lng - this.state.locality.lng) < 0.005) && (Math.abs(postbox.lat - this.state.locality.lat) < 0.005);
+    });
+    nearbyPostboxLocations.forEach((location) => {
+      this.showMarker(location);
+    });
   }
 
   render() {
